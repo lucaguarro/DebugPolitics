0086f6e91d26350f07c2060f5984843a47b6a4bd
Luca Guarro <luca.guarro@gmail.com>
2017-04-23 21:09:28 -0700

    adding html to every tweet

--- response.js
+++ response.js
@@ -1,9 +1,35 @@
+var cssId = 'myCss';  // you could encode the css path itself to generate id..
+/*if (!document.getElementById(cssId))
+{
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = '/assets/font-awesome.min.css';
+    link.media = 'all';
+    head.appendChild(link);
+}*/
+
 function injectFactCheck(){
-    var actionContainer = document.createElement('div');
-    actionContainer.style.cssText = "display: inline-block; min-width: 80px;"
+
     console.log("yessss");
     var theButton = document.createElement('a');
     theButton.style.cssText = "color: #aab8c2; display: inline-block; font-size: 16px; line-height: 1; padding: 0 2px; position: relative"
+    var iconContainer = document.createElement('div');
+    iconContainer.style.cssText = "display: inline-block";
+
+	var insertionarea = document.getElementsByClassName("ProfileTweet-actionList");
+    console.log(insertionarea);
+
+    for(var i = 0; i < insertionarea.length; i++){
+        var actionContainer = document.createElement('div');
+        actionContainer.style.cssText = "display: inline-block; min-width: 80px; border: 2px solid red"
+        console.log(i, insertionarea[i]);
+        var insertionpoint = insertionarea[i].parentNode;
+        insertionpoint.appendChild(actionContainer);        
+    }
+
 }
 
 chrome.runtime.onMessage.addListener(
