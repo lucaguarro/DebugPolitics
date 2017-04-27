90fb7d25857fabcb2d7ee880900c19e68768a325
Luca Guarro <luca.guarro@gmail.com>
2017-04-26 21:26:15 -0700

    click listeners to buttons

--- response.js
+++ response.js
@@ -10,6 +10,9 @@ if (!document.getElementById(cssId))
     link.media = 'all';
     head.appendChild(link);
 }*/
+
+
+
 var buzzWords = ["obama", "trump", "obamacare", "north korea", "politics", "china", "terrorism", "dems", "democrats", "healthcare", "president"];
 
 function getCNNlink(queryParams){
@@ -54,8 +57,7 @@ function readTweets(parentElements){
 }
 
 function injectFactCheck(){
-    var theButton = document.createElement('a');
-    theButton.style.cssText = "color: #aab8c2; display: inline-block; font-size: 16px; line-height: 1; padding: 0 2px; position: relative"
+
     var iconContainer = document.createElement('div');
     iconContainer.style.cssText = "display: inline-block";
 
@@ -67,18 +69,86 @@ function injectFactCheck(){
     console.log("kobe", politicalTweets[0][1]);
     for(var i = 0; i < politicalTweets.length; i++){
         var actionContainer = document.createElement('div');
-        actionContainer.style.cssText = "display: inline-block; min-width: 80px; border: 2px solid red"
+        actionContainer.style.cssText = "position: relative; display: inline-block;"
+        var button = document.createElement('button');
+        button.addEventListener('click', myFunction);
+        //button.className = "debugPoliticsButton";
+        button.style.cssText = "padding: 10px; font-size: 16px; border: 5px solid green; cursor: pointer;"
+        var items = document.createElement('div');
+        items.style.cssText = "position: inherit; background-color: #f9f9f9; min-width: 10px; z-index: 1;"
+        var list1 = document.createElement('a');
+        list1.style.cssText = "position: inherit; padding: 10px 10px; text-decoration: none; display: inline-block;"
+        var list2 = document.createElement('a');
+        list1.style.cssText = "position: inherit; padding: 10px 10px; text-decoration: none; display: inline-block;"
+        var list3 = document.createElement('a');
+        list1.style.cssText = "position: inherit; padding: 10x 10px; text-decoration: none; display: inline-block;"
         var insertionpoint = findClass(tweetParentsArray[politicalTweets[i][1]], "ProfileTweet-actionList").parentNode; //insertionarea[i].parentNode
-        insertionpoint.appendChild(actionContainer); 
+        insertionpoint.appendChild(actionContainer);
+        actionContainer.appendChild(button);
+        button.appendChild(items);
+        items.appendChild(list1);
+        items.appendChild(list2);
+        items.appendChild(list3); 
     }
-
     /*for(var i = 0; i < tweetParentsArray.length; i++){
         var actionContainer = document.createElement('div');
         actionContainer.style.cssText = "display: inline-block; min-width: 80px; border: 2px solid red"
         var insertionpoint = findClass(tweetParentsArray[i], "ProfileTweet-actionList").parentNode; //insertionarea[i].parentNode
         insertionpoint.appendChild(actionContainer);        
     }*/
+    //var url = createSearchUrl(words);
+    //var url = "https://eventregistry.org/json/article?ignoreKeywords=&keywords=Donald%20Trump&action=getArticles&resultType=articles&callback=JSON_CALLBACK&apiKey=6f51b1a6-3665-4487-97d2-1c72a2d6b617";
+    //guardian-key: a1928b80-4fac-4c41-82fe-4950f60933ad
+    //console.log(httpGet(url));
 
+    /*fetch(url)
+        .then(checkStatus)
+        .then(getJSON)
+        .then(function(data){
+            console.log('DATA', data);
+        })
+        .catch(function(err){
+            console.log('ERROR', err);
+        });*/
+}
+
+var myFunction = function(){
+    console.log("yolo swag kobeeee");
+}
+
+function createSearchUrl(words){
+    var url = "http://content.guardianapis.com/search?q=";
+    for(var i = 0; i < words.length; i++){
+        if(i != words.length - 1){
+            url = url + words[i] + "%20";
+        } else{
+            url = url + words[i]
+        }   
+    }
+    url = url + "&api-key=a1928b80-4fac-4c41-82fe-4950f60933ad";
+    return url; 
+}
+
+function checkStatus(response){
+    if(response.status === 200){
+        return Promise.resolve(response);
+    } else{
+        return Promise.reject(
+            new Error(response.statusText)
+        );
+    }
+}
+
+function getJSON(response){
+    return response.json();
+}
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
 }
 
 chrome.runtime.onMessage.addListener(
